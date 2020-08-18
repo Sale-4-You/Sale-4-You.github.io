ac6f2bd57b944341805c9e9405c3f46f775dec46
lcsvcn <lcs.vcn@gmail.com>
2020-08-18 12:20:32 -0300

    Updates

---
+++ precache-manifest.df15d5c98c373a1dc5c34cd32b7b1027.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b719db24631a9495f00eb42cf88ccd29",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "8394a52a051125d55ecd",
+    "url": "/home/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "913e11b69a37ca1aad63",
+    "url": "/home/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "e47184fab3074384b70e",
+    "url": "/home/static/css/main.a788d539.chunk.css"
+  },
+  {
+    "revision": "80e901e638c05d465e6f",
+    "url": "/home/static/js/2.f5a69a69.chunk.js"
+  },
+  {
+    "revision": "8394a52a051125d55ecd",
+    "url": "/home/static/js/3.ed122b59.chunk.js"
+  },
+  {
+    "revision": "913e11b69a37ca1aad63",
+    "url": "/home/static/js/4.2b4d6326.chunk.js"
+  },
+  {
+    "revision": "e47184fab3074384b70e",
+    "url": "/home/static/js/main.b540c4bc.chunk.js"
+  },
+  {
+    "revision": "96687aefe537b0c62660",
+    "url": "/home/static/js/runtime-main.8871f194.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/home/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/home/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/home/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "78ac7086663f24e1d0393a428bb677e1",
+    "url": "/home/static/media/ecommerce.78ac7086.svg"
+  },
+  {
+    "revision": "3fb6c2327533dd458692b704efe4d8d0",
+    "url": "/home/static/media/jannini.3fb6c232.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/home/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "f0eea7e6f5276138962b144bc4c9f06a",
+    "url": "/home/static/media/question.f0eea7e6.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/home/static/media/skill.34409398.svg"
+  }
+]);
